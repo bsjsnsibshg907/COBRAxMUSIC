fba9902ea8d0cbc346d9c0c456c3c913af7a0dd2
bsjsnsibshg907 <102121098+bsjsnsibshg907@users.noreply.github.com>
2023-05-24 15:54:05 +0300

    Update command.py

--- modules/helpers/command.py
+++ modules/helpers/command.py
@@ -1,8 +1,8 @@
 from typing import Union, List
 from pyrogram import filters
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group 
+other_filters2 = filters.private 
 
 
 def commandpro(commands: Union[str, List[str]]):
